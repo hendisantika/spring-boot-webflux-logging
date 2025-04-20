33e17f5db717817fd2390152977d98e5156d813c
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 19:53:35 +0700

    feat: Add logbookClientExchangeFunction in LogbookConfiguration🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/webfluxlogging/config/LogbookConfiguration.java
+++ src/main/java/id/my/hendisantika/webfluxlogging/config/LogbookConfiguration.java
@@ -2,9 +2,11 @@ package id.my.hendisantika.webfluxlogging.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.server.WebFilter;
 import org.zalando.logbook.Logbook;
 import org.zalando.logbook.autoconfigure.webflux.LogbookWebFluxAutoConfiguration;
+import org.zalando.logbook.spring.webflux.LogbookExchangeFilterFunction;
 import org.zalando.logbook.spring.webflux.LogbookWebFilter;
 
 /**
@@ -23,5 +25,10 @@ public class LogbookConfiguration {
     @Bean
     public WebFilter logbookFilter(Logbook logbook) {
         return new LogbookWebFilter(logbook);
+    }
+
+    @Bean
+    public ExchangeFilterFunction logbookClientExchangeFunction(final Logbook logbook) {
+        return new LogbookExchangeFilterFunction(logbook);
     }
 }
