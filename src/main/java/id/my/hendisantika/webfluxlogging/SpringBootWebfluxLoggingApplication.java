89a70fd300e6090d13c1f2758d156939de6d73ec
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 19:49:49 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/webfluxlogging/SpringBootWebfluxLoggingApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.webfluxlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxLoggingApplication.class, args);
+    }
+
+}
