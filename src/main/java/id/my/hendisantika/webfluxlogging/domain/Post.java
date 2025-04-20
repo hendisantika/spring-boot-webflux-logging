78c4f34ba1946919c66dc4f811e08c0d1089c166
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 19:55:12 +0700

    feat: Add Post Record🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/webfluxlogging/domain/Post.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.webfluxlogging.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-logging
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/04/25
+ * Time: 19.54
+ * To change this template use File | Settings | File Templates.
+ */
+public record Post(String title, String body, int userId) {
+}
