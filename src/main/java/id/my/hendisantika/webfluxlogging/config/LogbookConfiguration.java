849ece7d0f23c5ce36584dd558a1709c3e695c2d
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 19:52:59 +0700

    feat: Add LogbookConfiguration🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/webfluxlogging/config/LogbookConfiguration.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.webfluxlogging.config;
+
+import org.springframework.context.annotation.Import;
+import org.zalando.logbook.autoconfigure.webflux.LogbookWebFluxAutoConfiguration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-logging
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/04/25
+ * Time: 19.52
+ * To change this template use File | Settings | File Templates.
+ */
+@Import(LogbookWebFluxAutoConfiguration.class)
+public class LogbookConfiguration {
+}
