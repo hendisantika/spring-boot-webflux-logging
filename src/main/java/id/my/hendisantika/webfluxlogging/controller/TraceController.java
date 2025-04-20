e219c678560573bb2f93bda7c22374f4a6c70d6f
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 19:56:41 +0700

    feat: Add createPost in TraceController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/webfluxlogging/controller/TraceController.java
+++ src/main/java/id/my/hendisantika/webfluxlogging/controller/TraceController.java
@@ -1,14 +1,18 @@
 package id.my.hendisantika.webfluxlogging.controller;
 
 import id.my.hendisantika.webfluxlogging.domain.ApiResponse;
+import id.my.hendisantika.webfluxlogging.domain.Post;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.text.MessageFormat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -37,5 +41,16 @@ public class TraceController {
         var postResponse = createPost();
 
         return postResponse.flatMap(pt -> Mono.just(new ApiResponse(pt, "response")));
+    }
+
+    private Mono<Post> createPost() {
+        var post = new Post("foo", "bar", 1);
+
+        return webClient.post()
+                .uri(MessageFormat.format("{0}/posts", JSON_PLACEHOLDER_BASE_URL))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .body(Mono.just(post), Post.class)
+                .retrieve()
+                .bodyToMono(Post.class);
     }
 }
